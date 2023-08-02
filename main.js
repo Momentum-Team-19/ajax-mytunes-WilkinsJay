a16b4e1390be9c326bc3abde7453d40f92cd7234
Jaylan Wilkins <jay.wilkins23@yahoo.com>
2023-08-02 11:39:28 -0400

    css & main.js changes

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ function clear(container) {
 }
 
 form.addEventListener('submit', (event) => {
+    //prevents the page from reloading and prevents the form being sent to the wrong place as well
     event.preventDefault();
     clear(searchResults)
 
@@ -28,27 +29,37 @@ form.addEventListener('submit', (event) => {
     
     .then ((data) => {
         console.log(data.results)
-    
-        
-        for (let song of data.results) {
-            let resultBox = document.createElement('div')
-            resultBox.id = "resultBox"
-            
-            let imageDiv = document.createElement('img')
-            imageDiv.src = song.artworkUrl100
-            resultBox.append(imageDiv)
-            
-            let songDiv = document.createElement('div')
-            songDiv.innerText = song.trackName
-            resultBox.append(songDiv)
-    
-            searchResults.append(resultBox)
-            resultBox.addEventListener("click",() => {
-                musicPlayer.src = song.previewUrl;
-                musicPlayer.controls = true;
-                musicPlayer.preload = "auto";
-                
-            })
+// an empty array is truthy. Acts as if its true
+        if (data.results.length === 0) {
+            console.log('no results')
+            let messageDiv = document.createElement('div')
+            messageDiv.innerText = "No search results found"
+            searchResults.append(messageDiv)
         }
+        else {
+
+            
+            
+            for (let song of data.results) {
+                let resultBox = document.createElement('div')
+                resultBox.id = "resultBox"
+                
+                let imageDiv = document.createElement('img')
+                imageDiv.src = song.artworkUrl100
+                resultBox.append(imageDiv)
+                
+                let songDiv = document.createElement('div')
+                songDiv.innerText = song.trackName
+                resultBox.append(songDiv)
+                
+                searchResults.append(resultBox)
+                resultBox.addEventListener("click",() => {
+                    musicPlayer.src = song.previewUrl;
+                    musicPlayer.controls = true;
+                    musicPlayer.preload = "auto";
+                    
+                })
+            }
+        }
+        })
     })
-})
